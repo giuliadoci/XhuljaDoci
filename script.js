77717237c6fcdaf079c9f94d283356f988c68e72
giuliadoci <40279739+giuliadoci@users.noreply.github.com>
2018-07-05 00:46:57 +0200

    correzione finale
    correzione bung, aggiunta spinner, correzione dimensioni immagini

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 $(document).ready(function(){
 $('.contentlink').on('click', function(){
   var section = $(this).data('section');
+  $('.Banner').hide();
   $('.contents').hide();
-  $('#content_'+section).show();
-  if (section !== "home"){
-    $('.Banner').show();
-  } else {
-    $('.Banner').hide();
-  }
+  $('.loader-container').show();
+  setTimeout(function(){
+    $('#content_'+section).show();
+    $('.loader-container').hide();
+    if (section !== "home"){
+      $('.Banner').show();
+    } else {
+      $('.Banner').hide();
+    }
+  }, section !== "home" ? 1500 : 0);
 });
 });
+
+  /*il caricamento è ovviamente simulato per permettere di
+  vedere lo spinner
+ヽ(´▽`)/
+  */
